bcfc1d288c4059257bdefbbbc3ecccead3402937
Tharu_x <93185970+jonekenner@users.noreply.github.com>
2026-01-06 19:20:08 +0530

    Delete vpn-main/vpn-main directory

--- vpn-main/vpn-main/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting VPN Service..."
-echo ""
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo ""
-echo "Starting Flask application..."
-python app.py
-
